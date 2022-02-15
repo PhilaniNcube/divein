f9809fc2b72c79585da469c005f58b14c6e36123
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2022-02-15 11:11:07 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -214,24 +214,25 @@ const Index = () => {
               {loading ? 'Loading...' : 'Register'}
             </button>
           </div>
+ <div className="w-11/12 lg:w-9/12 pt-2 pb-12 sm:px-12">
+            <p className="font-bold text-md">POPIA COMPLIANT</p>
+            <p className="text-sm">
+              The Protection of Personal Information Act (POPI Act) comes into
+              effect on 1 July 2021 and to remain compliant with its provisions
+              SAIMI would like to give you, as its stakeholder, the opportunity
+              to unsubscribe should you wish to not receive any further
+              communication from SAIMI.
+            </p>
+            <p className="text-sm mt-1">
+              SAIMI regularly sends communication to its stakeholders to inform
+              them of any upcoming events and other industry related news. Your
+              email address forms part of the SAIMI stakeholders’ database and
+              is used for the purpose of communicating the abovementioned
+              information.
+            </p>
+          </div>
         </div>
-   <div className="w-11/12 lg:w-9/12 mx-auto py-2 px-3">
-          <p className="font-bold text-md">POPIA COMPLIANT</p>
-          <p className="text-sm">
-            The Protection of Personal Information Act (POPI Act) comes into
-            effect on 1 July 2021 and to remain compliant with its provisions
-            SAIMI would like to give you, as its stakeholder, the opportunity to
-            unsubscribe should you wish to not receive any further communication
-            from SAIMI.
-          </p>
-          <p className="text-sm mt-1">
-            SAIMI regularly sends communication to its stakeholders to inform
-            them of any upcoming events and other industry related news. Your
-            email address forms part of the SAIMI stakeholders’ database and is
-            used for the purpose of communicating the abovementioned
-            information.
-          </p>
-        </div>
+  
       </form>
     </div>
   );
